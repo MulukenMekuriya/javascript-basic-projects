4fae67c0225d54130b22dceebb4010164a20448d
Muluken Mekuriya <mulerthegrete@gmail.com>
2022-06-06 14:55:27 -0400

    color filliper done

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,6 +1,7 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 const btn = document.getElementById("btn");
+
 const color = document.getElementById("color");
 
 btn.addEventListener("click", function(){
